7c6049e1967dcac95f1277e8f750b1ef86bf308e
Василь Онуфрійчук <83518525+8a7l@users.noreply.github.com>
2026-08-22 13:07:26 +0300

    Update init.lua

--- lua_controller/init.lua
+++ lua_controller/init.lua
@@ -2,8 +2,29 @@ $events(true)
 $loopcycle(0)
 
 SERVER = "21725"
+ADMIN_SERVER = "21736"
+STATE_SERVER = "21737"
 
+-- Термінали
 T = $server_read(SERVER,"terminals")
 if not T then
 	T = Store()
+end
+
+-- Адміністратори
+A = $server_read(ADMIN_SERVER,"admins")
+if not A then
+	A = Store()
+end
+
+-- Перший адміністратор
+if not A.get("Fody") then
+	A.set("Fody",true)
+	$server_write(ADMIN_SERVER,"admins",A)
+end
+
+-- Стан mute
+M = $server_read(STATE_SERVER,"muted")
+if not M then
+	M = Store()
 end
